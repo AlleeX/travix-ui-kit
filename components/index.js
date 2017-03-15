8a1ce45e1d697cc673b59b9a51788c6a2499625d
Eduard_Trutsyk <ediktrutsik@gmail.com>
2017-03-15 20:58:17 +0300

    added collapse component

--- components/index.js
+++ components/index.js
@@ -3,9 +3,13 @@ import List from './list/list';
 import Price from './price/price';
 import RadioButton from './radioButton/radioButton';
 import Spinner from './spinner/spinner';
+import Collapse from './collapse/collapse';
+import CollapseItem from './collapseItem/collapseItem';
 
 export default {
   Button,
+  Collapse,
+  CollapseItem,
   List,
   Price,
   RadioButton,
